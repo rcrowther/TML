7c2acbee6fbae7f257fbc16a7460b0262bb0412c
rcrowther <rw.crowther@gmail.com>
2016-03-19 16:32:52 +0000

    ParserCompanion to unify companions

--- src/ParserCompanion.scala
+++ src/ParserCompanion.scala
@@ -1,16 +1,18 @@
 package tml
 
+
+
 trait ParserCompanion[PARSER <: Parser]
- {
+{
 
-/** Creates an instance of the parser for building.
-*/
-def builder(): PARSER
+  /** Creates an instance of the parser for building.
+    */
+  def builder(): PARSER
 
-/** Apply input, fixing errors, no error reporting.
-*
-* Default behaviour in Spec.
-*/
+  /** Apply input, fixing errors, no error reporting.
+    *
+    * Default behaviour in Spec.
+    */
   def apply(it: InputIterator)
   {
     val p = builder()
@@ -18,35 +20,35 @@ def builder(): PARSER
     p.blockBalance(fix = true)
     println("out:")
     println(s"'${p.result()}'")
-}
+  }
 
-/** Apply input, fixing errors, with error reporting.
-*/
+  /** Apply input, fixing errors, with error reporting.
+    */
   def errorReport(it: InputIterator)
   {
     val p = builder()
-p.logger = new ActiveLogger()
+    p.logger = new ActiveLogger()
     p(it)
     p.blockBalance(fix = true)
     println("out:")
     println(s"'${p.result()}'")
     println
     println(p.logger.toText())
-}
+  }
 
-/** Apply input, not fixing errors, with status reporting.
-*/
-/*
-  def profile(it: InputIterator)
-  {
-    val p = builder()
-    p(it)
-    p.blockBalance(fix = true)
-    println("out:")
-    println(s"'${p.result()}'")
-    println
-    println(p.logger.toText())
-}
-*/
+  /** Apply input, not fixing errors, with status reporting.
+    */
+  /*
+   def profile(it: InputIterator)
+   {
+   val p = builder()
+   p(it)
+   p.blockBalance(fix = true)
+   println("out:")
+   println(s"'${p.result()}'")
+   println
+   println(p.logger.toText())
+   }
+   */
 
 }//ParserCompanion
